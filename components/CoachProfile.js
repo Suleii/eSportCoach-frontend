ffc8ae1e25819bd6f25058e48391b22bd05c963a
Yasmine <Your email>
2023-12-11 16:43:26 +0100

    coach profile

---
+++ components/CoachProfile.js
@@ -0,0 +1,17 @@
+"use client";
+import { useDispatch, useSelector } from 'react-redux';
+import styles from '../styles/CoachProfile.module.css';
+
+
+function CoachProfile () {
+
+
+    return(
+        <div>
+            <h1>eSport Coach</h1>
+            <h2>Get professional coaching for your favorite game</h2>
+        </div>
+    )
+}
+
+export default CoachProfile
