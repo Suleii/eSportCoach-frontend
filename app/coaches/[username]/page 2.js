d176abb4ae57134c6bf8400a1a79b9cc1ecc8d4a
Yasmine <Your email>
2023-12-12 21:51:48 +0100

    deleted unused files

--- app/coaches/[username]/page 2.js
+++
@@ -1,7 +0,0 @@
-import CoachProfile from '../../../components/CoachProfile';
-
-function CoachProfilePage({params}) {
-  return <CoachProfile username={params.username}/>;
-}
-
-export default CoachProfilePage;
