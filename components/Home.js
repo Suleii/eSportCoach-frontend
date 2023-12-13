ae34a6e01e67c75baaca2d3d3ac15dd071f38e29
olivpl <oli.pala@outlook.fr>
2023-12-13 16:38:00 +0100

    login & home

--- components/Home.js
+++ components/Home.js
@@ -20,42 +20,78 @@ function Home () {
     };
 
 return (
-        
-            <div>
-                <a href="./login" className={styles.iconlogin}>
-                    <FontAwesomeIcon icon={faUser}/>
+        <body className=''>
+            <header className='mx-4 mt-5'>
+                <a href="./login" className='text-white flex justify-end mb-10'>
+                     <FontAwesomeIcon icon={faUser} className='bg-success h-10 w-10 rounded-full py-3'/>
                 </a>
-                    <h1>eSport Coach</h1>
-                    <h2>Get professional coaching for your favorite game</h2>
+                <h1 className='ml-8'>Hello eSport Coach</h1>
+            </header>
+                
+             <div className='mx-4 mt-5'>
                 <SearchBar onSearch={handleSearch} /> 
                 
             
-
-                <div className="carousel rounded-box">
+                <h1 className='mt-10 ml-8'>Top games</h1>
+                <div className="carousel carousel-center rounded-box flex space-x-4 h-64 mt-10 ">
                     <div className="carousel-item">
-                        <img src="../public/lol.jpeg" alt="League of Legends" />
+                        <img className=' w-40 rounded-lg' src="lol.jpg" alt="League of Legends" />
                     </div> 
                      <div className="carousel-item">
-                        <img src="https://daisyui.com/images/stock/photo-1565098772267-60af42b81ef2.jpg" alt="Burger" />
+                        <img className=' w-40 rounded-lg' src="fortnite.jpg" alt="Fortnite" />
                     </div> 
                     <div className="carousel-item">
-                        <img src="https://daisyui.com/images/stock/photo-1572635148818-ef6fd45eb394.jpg" alt="Burger" />
+                        <img className=' w-40 rounded-lg' src="apex.jpg" alt="Apex" />
                      </div> 
                      <div className="carousel-item">
-                        <img src="https://daisyui.com/images/stock/photo-1494253109108-2e30c049369b.jpg" alt="Burger" />
+                        <img className=' w-40 rounded-lg' src="https://daisyui.com/images/stock/photo-1494253109108-2e30c049369b.jpg" alt="Burger" />
                     </div> 
                      <div className="carousel-item">
-                        <img src="https://daisyui.com/images/stock/photo-1550258987-190a2d41a8ba.jpg" alt="Burger" />
+                        <img className=' w-40 rounded-lg' src="https://daisyui.com/images/stock/photo-1550258987-190a2d41a8ba.jpg" alt="Burger" />
                     </div> 
                      <div className="carousel-item">
-                        <img src="https://daisyui.com/images/stock/photo-1559181567-c3190ca9959b.jpg" alt="Burger" />
+                        <img className=' w-40 rounded-lg' src="https://daisyui.com/images/stock/photo-1559181567-c3190ca9959b.jpg" alt="Burger" />
                     </div> 
                      <div className="carousel-item">
-                        <img src="https://daisyui.com/images/stock/photo-1601004890684-d8cbf643f5f2.jpg" alt="Burger" />
+                        <img className=' w-40 rounded-lg' src="https://daisyui.com/images/stock/photo-1601004890684-d8cbf643f5f2.jpg" alt="Burger" />
+                    </div>
+                    <div className="carousel-item">
+                        <img className=' w-40 rounded-lg' src="https://daisyui.com/images/stock/photo-1601004890684-d8cbf643f5f2.jpg" alt="Burger" />
+                    </div>
+                    <div className="carousel-item">
+                        <img className=' w-40 rounded-lg' src="https://daisyui.com/images/stock/photo-1601004890684-d8cbf643f5f2.jpg" alt="Burger" />
                     </div>
                 </div>
+                
+                <h1 className='mt-10 ml-8'>
+                    Best Coaches
+                </h1>
+                <div className='mb-40'>
+                <h1 className='mt-10 ml-8'>
+                    Best Coaches
+                </h1>
+                <h1 className='mt-10 ml-8'>
+                    Best Coaches
+                </h1>
                 </div>
-        
+
+            </div>
+             
+            <footer className="footer p-10 bg-base-100 text-white flex flex-row space-x-40 items-center ">
+                <aside>
+                    <svg width="50" height="50" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fillRule="evenodd" clipRule="evenodd" className="fill-current items-center">
+                        <path d="M22.672 15.226l-2.432.811.841 2.515c.33 1.019-.209 2.127-1.23 2.456-1.15.325-2.148-.321-2.463-1.226l-.84-2.518-5.013 1.677.84 2.517c.391 1.203-.434 2.542-1.831 2.542-.88 0-1.601-.564-1.86-1.314l-.842-2.516-2.431.809c-1.135.328-2.145-.317-2.463-1.229-.329-1.018.211-2.127 1.231-2.456l2.432-.809-1.621-4.823-2.432.808c-1.355.384-2.558-.59-2.558-1.839 0-.817.509-1.582 1.327-1.846l2.433-.809-.842-2.515c-.33-1.02.211-2.129 1.232-2.458 1.02-.329 2.13.209 2.461 1.229l.842 2.515 5.011-1.677-.839-2.517c-.403-1.238.484-2.553 1.843-2.553.819 0 1.585.509 1.85 1.326l.841 2.517 2.431-.81c1.02-.33 2.131.211 2.461 1.229.332 1.018-.21 2.126-1.23 2.456l-2.433.809 1.622 4.823 2.433-.809c1.242-.401 2.557.484 2.557 1.838 0 .819-.51 1.583-1.328 1.847m-8.992-6.428l-5.01 1.675 1.619 4.828 5.011-1.674-1.62-4.829z"></path>
+                    </svg>
+                    
+                </aside> 
+                <div className='flex flex-row'>
+                    <p>Privacy</p> 
+                    <p>Contact</p>
+                    <p>Terms</p>
+                </div>
+                
+            </footer>
+        </body>
     )
 }
 
