4fcc62a467e1a539b6da4bf658a80e9cf8efbcba
Your name~Suleiman <Your email[201~suleiman.mulla1504@gmail.com>
2024-01-04 11:29:13 +0100

    folder username

--- [username]/page.js
+++
@@ -1,7 +0,0 @@
-import UserProfile from "../../../components/UserProfile";
-
-function UserProfilePage({ params }) {
-  return <UserProfile username={params.username} />;
-}
-
-export default UserProfilePage;
