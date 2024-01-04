2f791765d26d2cf7197f4bc3a7fdd8e02a8bf1c8
Your name~Suleiman <Your email[201~suleiman.mulla1504@gmail.com>
2024-01-04 10:46:21 +0100

    layout header

--- app/layout.js
+++ app/layout.js
@@ -1,29 +1,25 @@
-import { Inter } from 'next/font/google'
-import './globals.css'
-import StoreProvider from "./StoreProvider"
+import { Inter } from "next/font/google";
+import "./globals.css";
+import StoreProvider from "./StoreProvider";
 import { notFound } from "next/navigation";
-import Header from '../components/Header';
-import Footer from '../components/Footer';
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 
-
-
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'eSport Coach',
-  description: 'Get professional coaching',
+  title: "experience",
+  description: "Get professional coaching",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className='flex flex-col min-h-screen'>
+      <body className="flex flex-col min-h-screen">
         <StoreProvider>
-          
-            <Header/>
+          <Header />
           <main className="flex-auto">{children}</main>
-          <Footer/>
-        
+          <Footer />
         </StoreProvider>
       </body>
     </html>
